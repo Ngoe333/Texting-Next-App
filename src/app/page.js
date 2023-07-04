43564e9198deb506ee0f972e8c92e617e225ad35
Essouma Lovett <essoumangoelovett@gmail.com>
2023-07-04 07:43:26 +0100

    Setup Next App

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+
+// import Home from '@/app/home/page'
+// import About from '@/app/about/page'
+// import Contact from '@/app/contact/page'
+
+export default function Home() {
+  return (
+   <>
+      <h1>Food Website</h1>
+   
+   </>
+   
+  )
+};
